70f48d5889388b23426d38686e51a62e2aabfb48
Koyah <cryptomakoya@gmail.com>
2026-01-12 04:20:25 +0200

    Create VulnerableVault.sol

---
+++ src/VulnerableVault.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract VulnerableVault {
+    mapping(address => uint256) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        uint256 bal = balances[msg.sender];
+        require(bal > 0);
+
+        (bool sent, ) = msg.sender.call{value: bal}(""); // External call
+        require(sent, "Failed to send Ether");
+
+        balances[msg.sender] = 0; // State update (Too late!)
+    }
+}
